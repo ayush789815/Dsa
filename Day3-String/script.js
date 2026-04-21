ac893d6cdc6a702ac156bd2d9338b2514ee42b46
ayush789815 <ayushvishwakarmaa612@gmail.com>
2026-04-21 23:24:17 +0530

    Basic string question

---
+++ Day3-String/script.js
@@ -0,0 +1,22 @@
+// #1.  Length of Last Word
+// function lengthOfLastWord(s){
+//         let value = s.trim().split(" ")
+//         let lastIndex =  value.length-1
+//         console.log(value[lastIndex].length)
+// }
+// lengthOfLastWord("Hello World")
+
+
+ // #2 Valid Palindrome
+//  function valiPalindrome(s){
+//         let valid = s.toLowerCase().replace(/[^a-z0-9]/g,"")
+//         let reverse = valid.split().reverse().join("")
+//         console.log(reverse === valid) 
+// }
+    //  valiPalindrome("A man, a plan, a canal: Panama")
+//3 Reverse a String
+// function reverseString(s){
+//             let  reverse = s.split(" ").reverse().join(" ")
+//             console.log(reverse)
+// }
+// reverseString("A man, a plan, a canal: Panama")
